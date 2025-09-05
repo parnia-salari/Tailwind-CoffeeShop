76aa3d8b488ebf4a79771be59d0a7f21e4ff5f16
Parnia Salari <parniyasalari20@gmail.com>
2025-09-05 13:53:00 +0330

    completed mobile header and mobile nav

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,13 @@
-const toggleThemeBtn = document.querySelector("#toggle-theme");
-toggleThemeBtn.addEventListener("click" , () => {
+const toggleThemeBtn = document.querySelectorAll(".toggle-theme");
+const submenuOpen=document.querySelector(".submenu-open-btn");
+const submenu=document.querySelector(".submenu");
+const navOpneBtn=document.querySelector(".nav-icon")
+const navCloseBtn=document.querySelector(".navCloseBtn")
+const navPart=document.querySelector(".navPart")
+const overlay=document.querySelector(".overlay")
+
+toggleThemeBtn.forEach(btn=>{
+btn.addEventListener("click" , () => {
     if (localStorage.theme === "dark"){
         document.documentElement.classList.remove("dark");
         localStorage.theme = "light";
@@ -7,4 +15,31 @@ toggleThemeBtn.addEventListener("click" , () => {
         document.documentElement.classList.add("dark");
         localStorage.setItem("theme" , "dark");
     }
+    })
+})
+
+submenuOpen.addEventListener("click",(e)=>{
+    console.log(e.currentTarget.parentElement)
+    e.currentTarget.parentElement.classList.toggle("text-orange-300")
+    submenu.classList.toggle("submenu--open");
+})
+
+navOpneBtn.addEventListener("click" , ()=>{
+    navPart.classList.remove("-right-0")
+    navPart.classList.add("right-0")
+    overlay.classList.add("overlay--visible")
+})
+
+navCloseBtn.addEventListener("click",()=>{
+    navPart.classList.add("-right-0")
+    navPart.classList.remove("right-0")
+    overlay.classList.remove("overlay--visible")
+
+})
+
+overlay.addEventListener("click",()=>{
+    navPart.classList.add("-right-0")
+    navPart.classList.remove("right-0")
+    overlay.classList.remove("overlay--visible")
+
 })
